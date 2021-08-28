f5cb3d5ee9077b1e825603fd7593712319613aee
The6apcuk <32594531+The6apcuk@users.noreply.github.com>
2021-08-28 22:39:24 +0300

    Create array.h

---
+++ array_push_find_remove/array.h
@@ -0,0 +1,12 @@
+#define ERROR -1
+#define MAX_VAL 256
+
+int ar_push(double element);
+
+int ar_find_first(double element);
+
+int ar_remove(long element_idx);
+
+int list[MAX_VAL];
+
+long end_idx;
